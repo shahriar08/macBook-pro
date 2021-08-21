2770d79efb2272789f65adda18de8d2bc9096970
AL-Shahriar <anik7744@gmail.com>
2021-08-21 17:32:27 +0600

    Final submission,all function works well

--- js/app.js
+++ js/app.js
@@ -1,61 +1,65 @@
-function getAdditionSpec(price,addExtra){
+function getAdditionSpec(price, addExtra) {
     const addExtraCost = document.getElementById(addExtra + '-cost');
     addExtraCost.innerText = price;
+    // calculate total
     calculateTotal();
 }
 
-function getTotalCost(addExtra){
-    const extraCost = document.getElementById(addExtra+ '-cost').innerText;
+function getTotalCost(addExtra) {
+    const extraCost = document.getElementById(addExtra + '-cost').innerText;
     const productExtraCost = parseInt(extraCost);
     return productExtraCost;
 }
 
-function calculateTotal(){
+function calculateTotal() {
     const bestPrice = document.getElementById('bestPrice-cost').innerText;
-    const bestPriceCost = parseInt(bestPrice); 
+    const bestPriceCost = parseInt(bestPrice);
     const addExtraMemory = getTotalCost('memory');
     const addExtraStorage = getTotalCost('storage');
     const addDeliveryOption = getTotalCost('delivery');
     const TotalPrice = parseInt(bestPriceCost + addExtraMemory + addExtraStorage + addDeliveryOption);
+    // update on html
     document.getElementById('total-price').innerText = TotalPrice;
-} 
+    document.getElementById('totalCost-afterPromo').innerText = TotalPrice;
+}
 
-document.getElementById('promo-btn').addEventListener('click',function(){
+// Total cost after using promo 
+document.getElementById('promo-btn').addEventListener('click', function () {
     const promoInputField = document.getElementById('promo');
     const promoInput = promoInputField.value;
-
-        if(promoInput == 'stevekaku'){
-            const finalCost = document.getElementById('total-price').innerText;
-            const afterPromoDiscount = finalCost * 0.20;
-            const afterPromoCost = finalCost - afterPromoDiscount;
-            document.getElementById('totalCost-afterPromo').innerText = afterPromoCost;
-            promoInputField.value='';
-        }
-    else{
-        promoInputField.value='';
+    if (promoInput == 'stevekaku') {
+        const finalCost = document.getElementById('total-price').innerText;
+        const afterPromoDiscount = finalCost * 0.20;
+        const afterPromoCost = finalCost - afterPromoDiscount;
+        document.getElementById('totalCost-afterPromo').innerText = afterPromoCost;
+        promoInputField.value = '';
+    }
+    else {
+        promoInputField.value = '';
+        alert('Please use valid promo');
     }
 });
 
-
-document.getElementById('8gb-memory').addEventListener('click',function(){
-    getAdditionSpec(0,'memory');
+// Event handler 
+document.getElementById('8gb-memory').addEventListener('click', function () {
+    getAdditionSpec(0, 'memory');
 });
-document.getElementById('16gb-memory').addEventListener('click',function(){
-    getAdditionSpec(180,'memory');  
+document.getElementById('16gb-memory').addEventListener('click', function () {
+    getAdditionSpec(180, 'memory');
 })
-document.getElementById('256gb-storage').addEventListener('click',function(){
-    getAdditionSpec(0,'storage');
+document.getElementById('256gb-storage').addEventListener('click', function () {
+    getAdditionSpec(0, 'storage');
 });
-document.getElementById('512gb-storage').addEventListener('click',function(){
-    getAdditionSpec(100,'storage');
+document.getElementById('512gb-storage').addEventListener('click', function () {
+    getAdditionSpec(100, 'storage');
 });
-document.getElementById('1tb-storage').addEventListener('click',function(){
-    getAdditionSpec(180,'storage');
+document.getElementById('1tb-storage').addEventListener('click', function () {
+    getAdditionSpec(180, 'storage');
 });
-document.getElementById('free-delivery').addEventListener('click',function(){
-    getAdditionSpec(0,'delivery');
+document.getElementById('free-delivery').addEventListener('click', function () {
+    getAdditionSpec(0, 'delivery');
 });
-document.getElementById('paid-delivery').addEventListener('click',function(){
-    getAdditionSpec(20,'delivery');
+document.getElementById('paid-delivery').addEventListener('click', function () {
+    getAdditionSpec(20, 'delivery');
 });
 
